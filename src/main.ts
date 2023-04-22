8802f3f1e4c40fac2634880bc2bf91dfb3f8e66c
Joanna Liana <joanna_liana@outlook.com>
2023-04-22 19:26:19 +0200

    Add Swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { INestApplication } from '@nestjs/common';
+
+function setUpSwagger(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Median')
+    .setDescription('The Median API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
+}
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  setUpSwagger(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
