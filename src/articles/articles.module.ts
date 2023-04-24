6f8e108c9db170f48e0921869037fc29b6ae779a
Joanna Liana <joanna_liana@outlook.com>
2023-04-24 19:11:29 +0200

    Remove old articles controller

--- src/articles/articles.module.ts
+++ src/articles/articles.module.ts
@@ -1,12 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ArticlesService } from './articles.service';
-import { ArticlesController } from './api/articles.controller';
 import { PrismaModule } from '../prisma/prisma.module';
 import { DraftsController } from './api/drafts.controller';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [ArticlesController, DraftsController],
+  controllers: [DraftsController],
   providers: [ArticlesService],
 })
 export class ArticlesModule {}
