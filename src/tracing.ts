3070c5ecac86d171f721ff5518a82084a301b0a1
Joanna Liana <joanna_liana@outlook.com>
2023-05-04 18:13:28 +0200

    Add Nest tracing

--- src/tracing.ts
+++ src/tracing.ts
@@ -6,12 +6,17 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import * as process from 'process';
 import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
+import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 
 const traceExporter = new ConsoleSpanExporter();
 
 export const otelSDK = new NodeSDK({
   spanProcessor: new SimpleSpanProcessor(traceExporter),
-  instrumentations: [new HttpInstrumentation(), new ExpressInstrumentation()],
+  instrumentations: [
+    new HttpInstrumentation(),
+    new ExpressInstrumentation(),
+    new NestInstrumentation(),
+  ],
 });
 
 // gracefully shut down the SDK on process exit
