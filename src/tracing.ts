26e89f05e0c7d8aacbc4ab81196826aab3f6e02c
Joanna Liana <joanna_liana@outlook.com>
2023-05-05 08:09:45 +0200

    Add Jaeger (using the legacy library)

--- src/tracing.ts
+++ src/tracing.ts
@@ -8,10 +8,21 @@ import { HttpInstrumentation } from '@opentelemetry/instrumentation-http';
 import { ExpressInstrumentation } from '@opentelemetry/instrumentation-express';
 import { NestInstrumentation } from '@opentelemetry/instrumentation-nestjs-core';
 import { PrismaInstrumentation } from '@prisma/instrumentation';
+import { JaegerExporter } from '@opentelemetry/exporter-jaeger';
+import { Resource } from '@opentelemetry/resources';
+import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
 
-const traceExporter = new ConsoleSpanExporter();
+const traceExporter = process.env.SHOW_TRACES_IN_CONSOLE
+  ? new ConsoleSpanExporter()
+  : new JaegerExporter({ endpoint: 'http://localhost:14268/api/traces' });
+
+const resource = new Resource({
+  [SemanticResourceAttributes.SERVICE_NAME]: 'median',
+});
 
 export const otelSDK = new NodeSDK({
+  resource,
+  traceExporter,
   spanProcessor: new SimpleSpanProcessor(traceExporter),
   instrumentations: [
     new HttpInstrumentation(),
